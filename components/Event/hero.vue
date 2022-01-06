2be0558db50150bfc9b99133e095944c98dd02a9
Josia Almeida <josiaalmeida0001@gmail.com>
2022-01-06 21:41:15 +0100

    Create event page and assets

---
+++ components/Event/hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-4 mt-5">
+        <div class="card" style="width: 18rem">
+          <div class="card-body">
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-8">
+        <img src="/armariofundo.jpg" class="img-fluid" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/events.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <hero />
+  </div>
+</template>
+
+<script>
+import hero from '~/components/Event/hero.vue'
+export default {
+  name:"event",
+  components:{
+    hero
+  }
+}
+</script>
