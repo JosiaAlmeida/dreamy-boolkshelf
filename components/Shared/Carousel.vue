e061245bef7dcb5fd8d0ed42f553e3cd133d0e1a
Josia Almeida <josiaalmeida0001@gmail.com>
2022-01-07 12:42:15 +0100

    dynamically passing image and adding color to carousel elements

--- components/Shared/Carousel.vue
+++ components/Shared/Carousel.vue
@@ -1,8 +1,22 @@
 <template>
-  <div class="col-11 mt-3">
-    <VueSlickCarousel :rows="5" v-bind="slickOptions" :arrows="true" :dots="true">
-      <div div v-for="i in 15" :key="i">
-        <div class="cardContainer"></div>
+  <div class="col-12 mt-3">
+    <VueSlickCarousel
+      :responsive="['col-sm-12', 'col-md-4']"
+      :autoplay="true"
+      :rows="5"
+      :slidesToShow="3"
+      :arrows="false"
+      :dots="true"
+      :infinite="false"
+      :adaptiveHeight="true"
+    >
+      <div v-for="(img,i) in images" :key="i">
+        <div class="cardContainer d-flex align-items-end p-4" :style="imageProps(img)">
+          <div class="w-75">
+            <h6>Feira dos livos gigantesca no consulado português em Angola</h6>
+            <small class="text-small-grey">09 Agosto 21</small>
+          </div>
+        </div>
       </div>
     </VueSlickCarousel>
   </div>
@@ -17,20 +31,62 @@ export default {
   components: { VueSlickCarousel },
   data() {
     return {
-      slickOptions: {
-        slidesToShow: 3
-      }
+      images: [
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+        "/assets/img/armario.jpg",
+      ]
+    }
+  },
+  methods:{
+    imageProps(img){
+      return {'--backgroundimage': `url(${img})`}
+    },
+  },
+  computed: {
+    ColorBack(){
+      return {'--red-color': 'red'}
     }
   }
 }
 </script>
 
 <style scoped>
-.cardContainer{
+.cardContainer {
   width: 97%;
   height: 300px;
   border-radius: 10px;
   margin: 1%;
-  background-color: rgba(115, 123, 118, 0.9);
+  background: linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(99,110,106,0.8) 85%);
+  position: relative;
+  color: #fff;
+}
+.cardContainer::before{
+  content:'';
+  top:0;
+  left: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: var(--backgroundimage)no-repeat;
+  z-index: -1;
+}
+.text-small-grey {
+  color: #dce6e1;
 }
 </style>
