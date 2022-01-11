c3b2d28927575df5c05b36fa4997533200273aa2
reginaldo kambinda <34576252+unclereggie@users.noreply.github.com>
2022-01-11 21:11:02 +0100

    creating navbar

---
+++ components/Shared/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="nav">
+    <div class="">
+        <ul>
+            <li>
+                <a href="#">Sobre BD</a>
+            </li>
+            <li>
+                <a href="#">A estante dos sonhos</a>
+            </li>
+            <li>
+                <a href="#">Sala de arte</a>
+            </li>
+            <li>
+                <a href="#">Clube dos sonhos</a>
+            </li>
+            <li>
+                <a href="#">Eventos</a>
+            </li>
+            <li>
+                <div class="d-flex">
+                    <div class="social-netoworks">
+                        <a href="#"><i class="fa fa-pintrest"></i></a> 
+                        <a href="#"><i class="fa fa-instagram"></i></a>
+                    </div>
+                    <div class="lang-options">
+                        <a href="#">PT</a>
+                        <span>|</span>
+                        <a href="#">EN</a>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+</template>
