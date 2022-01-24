241e47cdf2a42873426a74a450ddf917898323e0
Josia Almeida <josiaalmeida0001@gmail.com>
2022-01-24 20:57:57 +0100

    Reduzindo o texto

--- pages/event/_slug.vue
+++ pages/event/_slug.vue
@@ -16,16 +16,11 @@
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap
         </p>
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard dummy text ever
           since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap
         </p>
       </div>
       <div class="col-md-7 mt-5 mb-5 d-flex justify-content-between">
