6998747408bb05cda268ebd6b3c9978de5f6a334
Josia Almeida <josiaalmeida0001@gmail.com>
2022-01-26 17:52:17 +0100

    ajustes na home

--- components/Home/DreamStocks.vue
+++ components/Home/DreamStocks.vue
@@ -12,11 +12,12 @@
   height: 100%;
   padding: 15% 10% 5% 10%;
   background-color: #dcdccb;
-  margin-top: -25%;
+  margin-top: -245px;
 }
 @media screen and (max-width: 600px) {
   .background {
     padding: 5%;
+    margin-top: -100px;
   }
 }
 </style>
