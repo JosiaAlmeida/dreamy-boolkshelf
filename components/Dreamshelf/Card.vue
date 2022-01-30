9a7604e71f99acee3be4af5f1bd8323ed59ec4d0
Josia Almeida <josiaalmeida0001@gmail.com>
2022-01-30 10:11:02 +0100

    Cards na estante dos sonhos

--- components/Dreamshelf/Card.vue
+++ components/Dreamshelf/Card.vue
@@ -1,1 +1,23 @@
-<template></template>
+<template>
+  <div class="card mb-3 card-container">
+    <img :src="img" class="card-img-top img-fluid img" />
+    <div class="card-body">
+      <h5 class="card-title text-center">Card title</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    ['img']: String,
+  },
+}
+</script>
+
+
+<style scoped>
+.card {
+  width: 98%;
+}
+</style>
