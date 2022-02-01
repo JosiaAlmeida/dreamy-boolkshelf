f7541e4effb6a8d0d6dfd8d9dab1e17f22ec7e3d
Josia Almeida <josiaalmeida0001@gmail.com>
2022-02-01 09:00:05 +0100

    Removendo imports desnecessario

--- components/Roomart/ShowPaint.vue
+++ components/Roomart/ShowPaint.vue
@@ -11,14 +11,9 @@
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-// optional style for arrows & dots
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
-  components: { VueSlickCarousel },
   props: {
-    ['showPaint']: Boolean,    
+    ['showPaint']: Boolean,
   },
   data() {
     return {
--- components/Shared/Carousel.vue
+++ components/Shared/Carousel.vue
@@ -22,12 +22,7 @@
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-// optional style for arrows & dots
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 export default {
-  components: { VueSlickCarousel },
   data() {
     return {
       options: {
