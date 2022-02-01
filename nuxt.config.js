1051e5f1b6b1a57b49d167886273faaf0c3ce1d1
Josia Almeida <josiaalmeida0001@gmail.com>
2022-02-01 08:32:38 +0100

    repondo plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    '~/plugins/flattenData.js',
+    '~/plugins/vue-slick-carousel.js'
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
