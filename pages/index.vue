ea998e6cab729c027ff048bcf640cc4d63e0d2a5
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-02-22 17:48:59 +0100

    removendo commit

--- pages/index.vue
+++ pages/index.vue
@@ -63,7 +63,6 @@ export default {
                     )
                 })
                 .catch((error) => error)
-            console.log(this.emAlta, this.filmes, this.events)
         },
     },
 }
