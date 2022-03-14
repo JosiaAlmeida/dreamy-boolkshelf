3128dbe0c9cc4e5193a7c5f5bbe647e1cb7c5ba5
JosiaAlmeida <josiaalmeida0001@gmail.com>
2022-03-14 21:03:12 +0100

    Restruturando modal e ajustando bug ao fechar props

--- components/Roomart/ShowPaint.vue
+++ components/Roomart/ShowPaint.vue
@@ -4,7 +4,7 @@
       <div class="row justify-content-center mt-5 heigth-100-vh">
         <div class="col-12 w-100 text-right">
           <button
-            @click="openModal()"
+            @click="closeModal()"
             class="btn btn-default btn-close text-white"
           >
             Fechar
@@ -14,10 +14,14 @@
           <div class="d-flex justify-content-around align-items-center w-100">
             <button
               type="button"
-              class="zIndex btn btn-default"
+              class="zIndex btn-default"
               @click="$refs.carouselRoomArts.prev()"
             >
-              <i class="fas fa-arrow-left" style="color: #fff0de"></i>
+              <img
+                src="/assets/img/left-arrow.svg"
+                class="img-fluid logo"
+                alt="Logo da dreamy"
+              />
             </button>
             <VueSlickCarousel
               v-if="Data.length"
@@ -25,27 +29,29 @@
               ref="carouselRoomArts"
               v-bind="options"
             >
-              <div v-for="(item, i) in getImages" :key="i">
+              <div v-for="(item, i) in Data" :key="i">
                 <div class="row justify-content-center align-items-center">
                   <div class="col-5 mt-5">
-                    <img :src="item" class="img-fluid img" alt="" />
+                    <img :src="item.url" class="img-fluid img" alt="" />
                   </div>
                   <div class="ml-4 col-5 separator2 d-flex">
-                    <!-- <div class="separator"></div> -->
-                    <!-- <hr class="separator" /> -->
                     <div class="p-4 text-white">
-                      <p>{{ getDescription[i] }}</p>
+                      <p>{{ description }}</p>
                     </div>
                   </div>
                 </div>
               </div>
             </VueSlickCarousel>
             <button
-              class="zIndex btn btn-default"
+              class="zIndex btn-default"
               type="button"
               @click="$refs.carouselRoomArts.next()"
             >
-              <i class="fas fa-arrow-right" style="color: #fff0de"></i>
+              <img
+                src="/assets/img/right-arrow.svg"
+                class="img-fluid logo"
+                alt="Logo da dreamy"
+              />
             </button>
           </div>
         </div>
@@ -58,7 +64,7 @@
 export default {
   props: {
     ['showPaint']: Boolean,
-    ['openModal']: Function,
+    ['closeModal']: Function,
     ['Data']: Array,
     ['description']: String,
   },
@@ -100,26 +106,9 @@ export default {
       },
     }
   },
-  computed: {
-    getImages() {
-      const item = this.Data.map((item) => item.images)
-      let allImage = []
-      for (let i = 0, length = item.length; i < length; i += 1) {
-        const element = item[i]
-        for (let j = 0, length = element.length; j < length; j += 1) {
-          // const element2 = element[j];
-          allImage.push(element[j])
-        }
-      }
-      return allImage
-    },
-    getDescription() {
-      return this.Data.map((item) => item.description)
-    },
-  },
-  mounted() {
-    console.log('Data', this.getDescription)
-  },
+  computed: {},
+  mounted() {},
+  destroyed() {},
 }
 </script>
 
@@ -133,7 +122,12 @@ export default {
 }
 button {
   color: #fff0de !important;
+  outline: none !important;
   /* border-bottom: 1px solid #fff0de; */
+}
+
+button:focus-visible {
+  outline: none !important;
 }
 
 .img {
@@ -160,6 +154,23 @@ hr {
 
 .zIndex {
   z-index: 1;
+  display: inline-block;
+  font-weight: 400;
+  color: #212529;
+  text-align: center;
+  vertical-align: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  background-color: transparent;
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  border-radius: 0.25rem;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 }
 
 .slick-arrow {
