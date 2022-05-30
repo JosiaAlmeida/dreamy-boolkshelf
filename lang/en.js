67a4a0b6ba076a88dea8a85d6ab762ffb81fd7f0
reginaldokambinda <reginaldo.kaminda@vinci-energies.net>
2022-05-30 22:01:24 +0100

    changes on interanationalization

--- lang/en.js
+++ lang/en.js
@@ -1,14 +1,41 @@
 export default {
   menu: {
     aboutBd: 'About BD',
-    dreams: 'The dream farm',
-    artRoom: 'Arts room',
-    dreamyClub: 'Dream club',
+    dreams: 'The Dreamy Bookshelf',
+    artRoom: 'Art Room',
+    dreamyClub: 'Dreamy club',
     events: 'Events',
   },
-  home: {},
+  home: {
+
+  },
+  whyDreamy:{
+    title:'Why the Dreamy Bookshelf?',
+    description1: `"Every reader's dreamm is to have their own book space for reading and for their books, but do you know where to start building your own bookshelf?`,
+    description2:'Where and how to buy your favorite books? Do you know the origin of the books that you read and many of the movies and series that you watch are based and classic and popular books? ',
+    description3:`Dreamy Bookshelf is here to help buy you're most desired books, help build your personal library and tell everything you need to knwo about the literature world "`
+  },
+  destaqueEstante:{
+    title: 'Destaques Estante de Sonho',
+    description: 'Learn more about the books world, find out which books are on the Dreamy Bookshelf and see how the the library is growing'
+  },
+  destaqueArt:{
+    title: 'Art Room',
+    description:'Enjoy and get inspried with minimalist and aesthetic scenarios from the book world'
+  },
+  destaqueEvents:{
+    title:'Events',
+    description:'Get updated about everything that is happening on the Books universe',
+  },
   arts:{
     title: 'Arts room',
     description: 'Discover, enjoy and be inspired by the aesthetic and minimalist settings of the book world'
+  },
+  about:{
+    title: 'Dreamy bookshelf is a space for books and photography lovers',
+    description: `In a country where the book access to a diverse literature is becoming a challenge , 
+    invest in a digital platform was a way to show that all of us can have acces to to books of every kind of genre, 
+    regardless of our tastes, reading can and should be a leisure more than a obligation than makes look smarter than others.`,
+    description2: 'Dreamy bookhslef aims to join in a place every book lover and share everything you need you need know about this world',
   }
 }
